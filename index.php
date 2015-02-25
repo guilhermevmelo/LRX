e53c469803d00bf6d2694c8b578d3b59f07d1440
Guilherme Vieira <guilhermevmelo@gmail.com>
2015-02-25 17:15:14 -0300

    Mudado para o PHPStorm. Adicionados mais métodos à classe Conexao.

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-<?php
- require_once("classes/Conexao.class.php");
- 
- $con = new Conexao();
- 
- $sql = "select * from users";
- $con->consultar($sql);
- 
-
- 
- ?>
+<!--
+  -- arquivo:   index.php
+  -- data:      25/02/2015
+  -- autor:     Guilherme Vieira
+  -- descrição: Página inicial da aplicação.
+  -->
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Laboratório de Raios-X | Solicitação de Análises</title>
+    <link rel="stylesheet" href="estilos/all.css" media="all">
+</head>
+<body>
+    <header role="presentation">Cabeçalho</header>
+    <main role="main">Principal</main>
+    <footer role="contentinfo">Rodapé</footer>
+</body>
+</html>
